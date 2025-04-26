9369c0a23bafa2bc34033a66a73131078b926572
anuragkainth <anuragkainth07@gmail.com>
2025-04-27 01:25:08 +0530

    feat: summary of leaves every minute

--- src/scheduler.js
+++ src/scheduler.js
@@ -15,7 +15,7 @@ function formatSummary(records) {
 }
 
 module.exports.start = (app) => {
-  cron.schedule('0 9 * * *', async () => {
+  cron.schedule('* * * * *', async () => {
     const today = dayjs().startOf('day');
     const upcoming = today.add(7, 'day');
     const records = await Leave.find({
